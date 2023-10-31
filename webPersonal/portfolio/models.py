abe401fc95c668ef4163df4c59bee319e0319f8e
Dragonf-j <55980623+Dragonf-j@users.noreply.github.com>
2023-10-31 22:40:00 +0100

    update 31/10/2023
    Cambios en ficheros de configuración

--- webPersonal/portfolio/models.py
+++ webPersonal/portfolio/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    descripcion = models.TextField()
-    image = models.ImageField()
-    created= models.TimeField(auto_now_add=True)
-    updated = models.TimeField(auto_now=True)
+    title = models.CharField(max_length=200, verbose_name= 'Titulo')
+    descripcion = models.TextField(verbose_name= 'Descripción')
+    image = models.ImageField(verbose_name= 'Imagen', upload_to='projects')
+    created= models.TimeField(auto_now_add=True, verbose_name= 'Fecha de creación')
+    updated = models.TimeField(auto_now=True, verbose_name= 'fecha de modificación')
+
+    class Meta:
+        verbose_name = 'proyecto'
+        verbose_name_plural ='proyectos'
+        ordering=[
+            '-created'
+        ]
+    def __str__(self):
+        return self.title
--- webPersonal/webPersonal/urls.py
+++ webPersonal/webPersonal/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +26,7 @@ urlpatterns = [
     path('porfolio/', views.portfolio, name='portfolio'),
     path('contact/', views.contacto, name='contact'),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
